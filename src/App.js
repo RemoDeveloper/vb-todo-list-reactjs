4d2c8083d2d1e8226505a1dd26cd3b2c0c08f75a
Remo <rameshphatak1118@gmail.com>
2023-02-15 23:13:07 +0530

    upload to-do-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Form  from './components/From';
+import TodoList  from './components/TodoList';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [input, setInput]= useState("");
+  const [todos, setTodos]= useState([]);
+  const [editTodo] = useState(null);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+       <Form 
+       input = {input}
+       setInput={setInput}
+       todos={todos}
+       setTodos={setTodos}
+       editTodo={editTodo}
+       setEditTodo={editTodo}
+
+       />
+       </div>
+       <TodoList todos={todos} setTodos={setTodos} 
+       setEditTodo={editTodo}/>
+      </div>
+      
+
     </div>
+   
   );
 }
 
